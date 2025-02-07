1e6496548bb99ac1e68c812183ab261eb445d946
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-02-07 16:00:01 +0700

    likes service and sample.ts

--- perfect_date_be_main/src/modules/activities/activities.module.ts
+++ perfect_date_be_main/src/modules/activities/activities.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Activity, ActivitySchema } from './schemas/activity.schema';
+import { ActivitiesRepository } from './activities.repo';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Activity.name,
+        schema: ActivitySchema,
+      },
+    ])
+  ],
   controllers: [ActivitiesController],
-  providers: [ActivitiesService],
+  providers: [ActivitiesService, ActivitiesRepository],
 })
-export class ActivitiesModule {}
+export class ActivitiesModule { }
